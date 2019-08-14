dd5fffdf56420d190513b031462c808d67fa1a14
himanshu619 <himanshu@7continentsmedia.com>
2019-08-14 15:23:10 +0530

    first vue project

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Himanshu first project in vue"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
